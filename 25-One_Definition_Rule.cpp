832520ec1c7d17ada3c6489f76560b49cbc78e36
Keanu-Type <keanuberchestype0@gmail.com>
2022-11-26 10:50:31 +0800

    One Definition Rule

---
+++ 25-One_Definition_Rule.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//Variable: Delarationdefinition;
+//even if you make file(on same path); it will not able to redeclare weight{}; this is one definition rule
+//the compilir cant choose so it will throw an error
+//include function
+double weight {};
+
+int main(){
+    
+}
